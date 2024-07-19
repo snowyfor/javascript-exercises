a9b3041aea234fbddd7613075cfd41ac6197e96f
Akmal Fauzan Azima <134951391+snowyfor@users.noreply.github.com>
2024-07-19 10:15:37 +0700

    Complete exercises 08 - 12

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	//if(arr.length == 0) return 0; replaced by '0' in reduce second parameter
+  return arr.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr.length == 0) return 0;
+  return arr.reduce((total, num) => total * num);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  // shortcut: return Math.pow(a,b);
+  let result = 1;
+	for(let i = 0; i < b; i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0) return 1
+  let result = 1;
+  for(let i = num; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (arr) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    // clean string (no punctiation, no space, just alphanumerical)
+    const cleanedString = arr.toLowerCase()
+                            .split('')
+                            .filter((character => alphanumerical.includes(character)))
+                            .join('');
+
+    const reversedString = cleanedString.split('')
+                                        .reverse()
+                                        .join('');
+
+    return (reversedString === cleanedString);
 };
 
 // Do not edit below this line
